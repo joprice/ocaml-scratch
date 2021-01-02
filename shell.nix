4d3004a230f6366c7a1921a1f9d880e105085772
Joseph Price <pricejosephd@gmail.com>
2021-01-02 11:11:12 -0500

    try updated ppx_deriving

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 let
   pkg-sources = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
-    sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
+    url = "https://github.com/sternenseemann/nixpkgs/archive/3823e9790bd4bb51a3578ecfef3a7321b741be1d.tar.gz";
+    sha256 = "1fd9fllv8p31y0nlswp2rblhdpkih4v8i58wszp0xacbqqapax09";
+    #url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
+    #sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
   };
   pkgs = import pkg-sources { };
 in
